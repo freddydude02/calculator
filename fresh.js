f9e8531f86373e8bd49585720c24015d9152113e
freddydude02 <frederickkeke@gmail.com>
2021-12-02 18:26:36 +1100

    modulising

--- fresh.js
+++ fresh.js
@@ -1,81 +1,41 @@
-import { calculate } from "./calculate.js";
-// import { btnCheck } from "./btnCheck.js";
-import { numberFunc } from "./functionCollection.js";
+import {
+    numberFunc,
+    operatorFunc,
+    clearFunc,
+    equalsFunc,
+    dotFunc,
+} from "./functionCollection.js";
+
 const log = (item) => console.log(item);
+const allBtn = document.querySelectorAll(".calc__btn");
 const onPageLoad = () => {
-    const calculator = document.querySelector(".calc");
-    const allBtn = document.querySelectorAll(".calc__btn");
-    const display = document.querySelector(".calc__display");
-
-    const numberFunc = (clickedBtn) => {
-        if (
-            display.innerText === "0" ||
-            (calculator.dataset.prevKeyType === "operator" &&
-                display.innerText !== "0.")
-        ) {
-            display.innerText = clickedBtn.target.innerText;
-        } else {
-            display.innerText = display.innerText + clickedBtn.target.innerText;
-        }
-        calculator.dataset.prevKeyType = "number";
-    };
-    const operatorFunc = (clickedBtn) => {
-        clickedBtn.target.classList.add("active");
-        calculator.dataset.prevKeyType = "operator";
-        calculator.dataset.firstValue = display.innerText;
-        calculator.dataset.operator = clickedBtn.target.dataset.action;
-    };
-    const clearFunc = (clickedBtn) => {
-        display.innerText = 0;
-        calculator.dataset.firstValue = 0;
-    };
-    const equalsFunc = (clickedBtn) => {
-        const firstValue = calculator.dataset.firstValue;
-        const operator = calculator.dataset.operator;
-        const SecondValue = display.innerText;
-        if (calculate(firstValue, operator, SecondValue)) {
-            display.innerText = calculate(firstValue, operator, SecondValue);
-        }
-    };
-    const dotFunc = (clickedBtn) => {
-        log(calculator.dataset.prevKeyType);
-        if (!display.innerText.includes(".")) {
-            display.innerText = display.innerText + ".";
-        }
-        if (calculator.dataset.prevKeyType === "operator") {
-            display.innerText = "0.";
-        }
-    };
-    const btnCheck = (checkedBtn) => {
-        checkedBtn.forEach((btn) => {
-            btn.addEventListener("click", (clickedBtn) => {
-                checkedBtn.forEach((btn) => {
-                    btn.classList.remove("active");
-                });
-
-                if (!clickedBtn.target.dataset.action) {
-                    numberFunc(clickedBtn);
-                }
-                if (
-                    clickedBtn.target.dataset.action === "plus" ||
-                    clickedBtn.target.dataset.action === "minus" ||
-                    clickedBtn.target.dataset.action === "times" ||
-                    clickedBtn.target.dataset.action === "slash"
-                ) {
-                    operatorFunc(clickedBtn);
-                }
-                if (clickedBtn.target.dataset.action === "clear") {
-                    clearFunc(clickedBtn);
-                }
-                if (clickedBtn.target.dataset.action === "equals") {
-                    equalsFunc(clickedBtn);
-                }
-                if (clickedBtn.target.dataset.action === "dot") {
-                    dotFunc(clickedBtn);
-                }
+    allBtn.forEach((btn) => {
+        btn.addEventListener("click", (clickedBtn) => {
+            allBtn.forEach((btn) => {
+                btn.classList.remove("active");
             });
+
+            if (!clickedBtn.target.dataset.action) {
+                numberFunc(clickedBtn);
+            }
+            if (
+                clickedBtn.target.dataset.action === "plus" ||
+                clickedBtn.target.dataset.action === "minus" ||
+                clickedBtn.target.dataset.action === "times" ||
+                clickedBtn.target.dataset.action === "slash"
+            ) {
+                operatorFunc(clickedBtn);
+            }
+            if (clickedBtn.target.dataset.action === "clear") {
+                clearFunc(clickedBtn);
+            }
+            if (clickedBtn.target.dataset.action === "equals") {
+                equalsFunc(clickedBtn);
+            }
+            if (clickedBtn.target.dataset.action === "dot") {
+                dotFunc(clickedBtn);
+            }
         });
-    };
-    btnCheck(allBtn);
+    });
 };
 onPageLoad();
--- functionCollection.js
+++ functionCollection.js
@@ -1,3 +1,7 @@
+import { calculate } from "./calculate.js";
+const calculator = document.querySelector(".calc");
+const display = document.querySelector(".calc__display");
+
 export const numberFunc = (clickedBtn) => {
     if (
         display.innerText === "0" ||
@@ -9,4 +13,31 @@ export const numberFunc = (clickedBtn) => {
         display.innerText = display.innerText + clickedBtn.target.innerText;
     }
     calculator.dataset.prevKeyType = "number";
+};
+export const operatorFunc = (clickedBtn) => {
+    clickedBtn.target.classList.add("active");
+    calculator.dataset.prevKeyType = "operator";
+    calculator.dataset.firstValue = display.innerText;
+    calculator.dataset.operator = clickedBtn.target.dataset.action;
+};
+export const clearFunc = (clickedBtn) => {
+    display.innerText = 0;
+    calculator.dataset.firstValue = 0;
+};
+export const equalsFunc = (clickedBtn) => {
+    const firstValue = calculator.dataset.firstValue;
+    const operator = calculator.dataset.operator;
+    const SecondValue = display.innerText;
+    if (calculate(firstValue, operator, SecondValue)) {
+        display.innerText = calculate(firstValue, operator, SecondValue);
+    }
+};
+export const dotFunc = (clickedBtn) => {
+    log(calculator.dataset.prevKeyType);
+    if (!display.innerText.includes(".")) {
+        display.innerText = display.innerText + ".";
+    }
+    if (calculator.dataset.prevKeyType === "operator") {
+        display.innerText = "0.";
+    }
 };
