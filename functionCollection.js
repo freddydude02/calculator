fb2719eb60633b51d58159eb8ef0055e9ee142b7
freddydude02 <frederickkeke@gmail.com>
2021-12-02 22:07:09 +1100

    almost-there

--- functionCollection.js
+++ functionCollection.js
@@ -1,4 +1,6 @@
 import { calculate } from "./calculate.js";
+
+const log = (item) => console.log(item);
 const calculator = document.querySelector(".calc");
 const display = document.querySelector(".calc__display");
 
@@ -15,25 +17,57 @@ export const numberFunc = (clickedBtn) => {
     calculator.dataset.prevKeyType = "number";
 };
 export const operatorFunc = (clickedBtn) => {
+    const firstValue = calculator.dataset.firstValue;
+    const operator = calculator.dataset.operator;
+    const SecondValue = display.innerText;
+    if (
+        firstValue &&
+        operator &&
+        calculator.dataset.prevKeyType !== "operator" &&
+        calculator.dataset.prevKeyType !== "equals"
+    ) {
+        display.innerText = calculate(firstValue, operator, SecondValue);
+    }
+
     clickedBtn.target.classList.add("active");
     calculator.dataset.prevKeyType = "operator";
     calculator.dataset.firstValue = display.innerText;
     calculator.dataset.operator = clickedBtn.target.dataset.action;
 };
-export const clearFunc = (clickedBtn) => {
-    display.innerText = 0;
-    calculator.dataset.firstValue = 0;
-};
+export const clearFunc = (clickedBtn) => {};
+
 export const equalsFunc = (clickedBtn) => {
     const firstValue = calculator.dataset.firstValue;
     const operator = calculator.dataset.operator;
     const SecondValue = display.innerText;
-    if (calculate(firstValue, operator, SecondValue)) {
-        display.innerText = calculate(firstValue, operator, SecondValue);
+
+    if (firstValue) {
+        if (calculator.dataset.prevKeyType !== "equals") {
+            calculator.dataset.previousSecondValue = SecondValue;
+            log(
+                (calculator.dataset.previousSecondValue = SecondValue) +
+                    "hi this is bind"
+            );
+        }
+        display.innerText = calculator.dataset.firstValue = calculate(
+            firstValue,
+            operator,
+            calculator.dataset.previousSecondValue
+        );
+        log(firstValue);
+        log(operator);
+        log(calculator.dataset.previousSecondValue);
+        log(
+            calculate(
+                firstValue,
+                operator,
+                calculator.dataset.previousSecondValue
+            )
+        );
+        calculator.dataset.prevKeyType = "equals";
     }
 };
 export const dotFunc = (clickedBtn) => {
-    log(calculator.dataset.prevKeyType);
     if (!display.innerText.includes(".")) {
         display.innerText = display.innerText + ".";
     }
